689c7f4cf361f5c4842272eaf09b692d4b345696
Avronil <basuavronil65@gmail.com>
2026-04-16 11:23:42 +0530

    Update scrambler_downstream.v

--- scrambler_downstream.v
+++ scrambler_downstream.v
@@ -1,21 +1,14 @@
 module scrambler_downstream (
-    input  wire        clk,
-    input  wire        rst_n,
-    input  wire        enable,
-    input  wire [1:0]  data_in,  // 2-bit input (Even/Odd pair)
-    output wire [1:0]  data_out  // 2-bit scrambled output
+    input  clk, rst,  enable,
+    input [1:0]  data_in,  // 2-bit input (Even/Odd pair)
+    output [1:0]  data_out  // 2-bit scrambled output
 );
 
     reg [22:0] lfsr;
     wire s0, s1;
 
-    // S0 is calculated from current state
     assign s0 = lfsr[22] ^ lfsr[17];
-
-    // S1 is calculated by "looking ahead" to what the taps 
-    // would be after 1 shift. 
-    // Note: lfsr[21] becomes the new [22], lfsr[16] becomes the new [17]
-    assign s1 = lfsr[21] ^ lfsr[16];
+    assign s1 = lfsr[2] ^ lfsr[5];
 
     // Parallel Scrambling
     assign data_out[0] = (enable) ? (data_in[0] ^ s0) : data_in[0];
@@ -25,8 +18,6 @@ module scrambler_downstream (
         if (!rst_n) begin
             lfsr <= 23'h00001;
         end else if (enable) begin
-            // Parallel shift by 2 positions
-            // We push the two feedback bits into the bottom 2 bits
             lfsr <= {lfsr[20:0], s1, s0};
         end
     end
